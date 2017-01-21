660a9fa42f6fcf8b69145f06674bf645dd88028e
Steven <SeiryuZ@hotmail.com>
2017-01-21 17:29:09 +0700

    More basic configuration

--- magnet/settings.py
+++ magnet/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'magnet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'magnet', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'magnet', 'static')]
---
+++ magnet/static/js/main.js
@@ -0,0 +1,1 @@
+console.alert("Welcome to magnet website");
