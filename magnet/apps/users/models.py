3f49d37c8622d4eb02faf0d3e75ce8212443ee26
Steven <stvn.bu@gmail.com>
2017-01-21 16:46:37 +0700

    More fields for user model

--- magnet/apps/users/models.py
+++ magnet/apps/users/models.py
@@ -7,18 +7,30 @@ from django.db import models
 class User(AbstractBaseUser):
     email = models.CharField(max_length=40, unique=True)
     name = models.CharField(max_length=255)
-    mobile_number = models.CharField(max_length=255)
+    mobile_number = models.CharField(max_length=255, unique=True,
+                                     help_text="ex: +6281xxxxxxx")
+    whatsapp_number = models.CharField(max_length=255, unique=True,
+                                       help_text="ex: +6281xxxxxx")
+    pk_number = models.PositiveSmallIntegerField()
+    previous_university = models.CharField(max_length=255, blank=True, null=True)
 
     @unique
     class Gender(Enum):
         MALE = 1
         FEMALE = 2
-    gender = models.SmallIntegerField(choices=(
-        (g.value, g.name.title())for g in Gender)
+    gender = models.SmallIntegerField(choices=
+        ((g.value, g.name.title()) for g in Gender)
     )
 
-    is_active = models.BooleanField()
+    @unique
+    class Type(Enum):
+        VOLUNTEER = 1
+        INITIATOR = 2
+    type = moderls.SmallIntegerField(choices=
+        ((t.value, t.name.title()) for t in Type)
+    )
 
+    is_active = models.BooleanField(default=False)
 
 
     USERNAME_FIELD = 'email'
@@ -30,5 +42,9 @@ class User(AbstractBaseUser):
 
     def get_short_name(self):
         return self.name
+
+    @property
+    def first_name(self):
+        return self.name.split(" ")[0]
 
 
