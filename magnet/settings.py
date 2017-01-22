c31e429be2a2cca493cce4ace2caaa58a07b2b26
Steven <stvn.bu@gmail.com>
2017-01-22 15:06:20 +0700

    Added middleware to change default language

--- magnet/settings.py
+++ magnet/settings.py
@@ -51,6 +51,10 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    'magnet.core.middleware.ForceDefaultLanguageMiddleware',
+
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
